55ffc8f864d1d9e3aabbe90abbd3b1863af61bfb
Janina626 <japr4229@bht-berlin.de>
2026-01-07 16:51:45 +0100

    StreamingAudioBookCount

--- lab03/src/main/java/de/bht/pr2/lab03/part3/StreamingAudioBookCount.java
+++ lab03/src/main/java/de/bht/pr2/lab03/part3/StreamingAudioBookCount.java
@@ -1,5 +1,10 @@
 package de.bht.pr2.lab03.part3;
 
+import de.bht.pr2.lab03.books.AudioBook;
+import de.bht.pr2.lab03.books.Book;
+import de.bht.pr2.lab03.parser.BookParser;
+import de.bht.pr2.lab03.store.BookStore;
+
 public class StreamingAudioBookCount {
 
   public static void main(String[] args) {
@@ -8,5 +13,18 @@ public class StreamingAudioBookCount {
     Anzahl Hoerbuecher per Streaming: 5
 
      */
+    int count = 0;
+
+    for (String s : BookStore.getSoldBooks()) {
+      Book b = BookParser.parse(s);
+
+      if (b instanceof AudioBook) {
+        AudioBook a = (AudioBook) b;
+        if (a.getPlayMode().equalsIgnoreCase("Streaming")) {
+          count++;
+        }
+      }
+    }
+    System.out.println("Anzahl Hoerbuecher per Streaming: " + count);
   }
 }
