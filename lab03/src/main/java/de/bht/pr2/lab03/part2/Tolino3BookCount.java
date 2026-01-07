0553ad0aaa946e7c939e66e4e30c2ebd32fc6fe0
Janina626 <japr4229@bht-berlin.de>
2026-01-07 16:47:19 +0100

    Tolino3BookCount

--- lab03/src/main/java/de/bht/pr2/lab03/part2/Tolino3BookCount.java
+++ lab03/src/main/java/de/bht/pr2/lab03/part2/Tolino3BookCount.java
@@ -1,5 +1,10 @@
 package de.bht.pr2.lab03.part2;
 
+import de.bht.pr2.lab03.books.Book;
+import de.bht.pr2.lab03.books.EBook;
+import de.bht.pr2.lab03.parser.BookParser;
+import de.bht.pr2.lab03.store.BookStore;
+
 public class Tolino3BookCount {
 
   public static void main(String[] args) {
@@ -8,5 +13,18 @@ public class Tolino3BookCount {
     Anzahl Ebuecher bei Tolino 3: 9
 
      */
+    int count = 0;
+
+    for (String s : BookStore.getSoldBooks()) {
+      Book b = BookParser.parse(s);
+
+      if (b instanceof EBook) {
+        EBook e = (EBook) b; // b vom Typ Buch wird zum Typ EBook umgewandelt
+        if (e.getTolinoVersion().equals("3")) {
+          count++;
+        }
+      }
+    }
+    System.out.println("Anzahl Ebuecher bei Tolino 3: " + count);
   }
 }
